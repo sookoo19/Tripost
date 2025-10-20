814d93aef7091da155c46b39ff26ab11714d9bfa
sookoo19 <kouhei20001011@gmail.com>
2025-10-20 19:19:14 +0100

    下書き編集画面修正２

--- backend_tripost/app/Http/Requests/PostRequest.php
+++ backend_tripost/app/Http/Requests/PostRequest.php
@@ -46,6 +46,8 @@ class PostRequest extends FormRequest
 
             'photos' => 'nullable|array|max:8',
             'photos.*' => 'file|image|max:10240',
+            'existing_photos' => 'sometimes|array',
+            'existing_photos.*' => 'string',
         ];
         
     }
