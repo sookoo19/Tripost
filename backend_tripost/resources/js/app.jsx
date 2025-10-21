bed03629918a97439152e562afbb3eec3c356da2
sookoo19 <kouhei20001011@gmail.com>
2025-10-21 12:03:54 +0100

    アプリ名修正

--- backend_tripost/resources/js/app.jsx
+++ backend_tripost/resources/js/app.jsx
@@ -6,7 +6,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { Inertia } from '@inertiajs/inertia';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Tripost';
 
 createInertiaApp({
   title: title => `${title} - ${appName}`,
